84656f25a17e8eb9e126bc58e616ae94c226ca73
seldonatreides <seldonatreides@gmail.com>
2020-04-19 13:22:43 -0500

    Created OAuth2 login page for GitHub credentials.

---
+++ src/test/java/org/launchcode/picatta/ForestRobertsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.picatta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForestRobertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
