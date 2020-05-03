2b1e78b6c9e68ef5284230233f078087fd7e20b1
seldonatreides <seldonatreides@gmail.com>
2020-05-03 11:20:06 -0500

    Implemented OAuth2 login with Google and Github. Added Login and Signup page with email. User accounts stored in UserRepository.

---
+++ src/main/java/org/launchcode/picatta/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package org.launchcode.picatta.repository;
+
+import org.launchcode.picatta.data.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+}
---
+++ src/main/java/org/launchcode/picatta/security/oauth2/user/OAuth2UserInfoFactory.java
@@ -0,0 +1,19 @@
+package org.launchcode.picatta.security.oauth2.user;
+
+import org.launchcode.picatta.exception.OAuth2AuthenticationProcessingException;
+import org.launchcode.picatta.data.AuthProvider;
+
+import java.util.Map;
+
+public class OAuth2UserInfoFactory {
+
+    public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) {
+        if(registrationId.equalsIgnoreCase(AuthProvider.google.toString())) {
+            return new GoogleOAuth2UserInfo(attributes);
+        } else if (registrationId.equalsIgnoreCase(AuthProvider.github.toString())) {
+            return new GithubOAuth2UserInfo(attributes);
+        } else {
+            throw new OAuth2AuthenticationProcessingException("Sorry! Login with " + registrationId + " is not supported yet.");
+        }
+    }
+}
